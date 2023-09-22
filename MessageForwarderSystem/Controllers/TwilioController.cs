b1ed485ad6543c308b895ddbb1b851e95dadf6d5
li,xiaokun <leeli@iu.edu>
2023-09-21 20:36:51 -0400

    Project Init

---
+++ MessageForwarderSystem/Controllers/TwilioController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using MessageForwarderSystem.Models;
+
+namespace MessageForwarderSystem.Controllers;
+
+/// <summary>
+/// Class <c>TwilioController</c> Controller class responsible for handling incoming SMS messages from Twilio.
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class TwilioController : ControllerBase
+{
+    private readonly ILogger<TwilioController> _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TwilioController"/> class.
+    /// </summary>
+    /// <param name="logger">The logger instance for logging.</param>
+    public TwilioController(ILogger<TwilioController> logger)
+    {
+        _logger = logger;
+    }
+    /// <summary>
+    /// Method <c>PostTwilioMessage</c>: Twilio SMS webhook endpoint.
+    /// This method is called by Twilio when a message is sent to the Twilio number.
+    /// </summary>
+    /// <param name="twilioMessage">An instance of the TwilioMessage class containing message data.</param>
+    /// <returns>An HTTP action result indicating the status of the operation.</returns>
+    [HttpPost]
+    public ActionResult PostTwilioMessage(TwilioMessage twilioMessage)
+    {
+        _logger.LogInformation(twilioMessage.ToString());
+        return Ok();
+    }
+}
---
+++ Reports/CapstoneMinutes.md
@@ -0,0 +1,52 @@
+# No Kill Louisville Check in Service Minutes
+
+**Members:**
+- Xiaokun Li
+- Sebastian
+- Jonathan Roberts
+
+## September 2 Meeting
+- **Time:** 1:00pm to 3:30pm
+- **Topics:** 
+  - Meeting with No Kill Louisville
+  - Requirements Gathering
+  - Discussed potential app solutions
+
+## September 5th Meeting
+- **Time:** 8:00pm to 8:30pm
+- **Topics:**
+  - Ideas for the app
+  - Look at work done by previous group
+
+## September 13th Meeting
+- **Time:** 7:15pm to 7:40pm
+- **Topics:** 
+  - Switch from app to text check-in service
+  - Set up GitHub repo
+  - Needs API key for acuity
+
+## September 25th Meeting
+- **Time:** 8:00pm to 8:45pm
+- **Topics:** 
+  - Discuss services that will be used
+  - Look through Acuity Documentation
+  - Divide out work and build proof of concept
+
+## October 9th Meeting
+- **Time:** 8:00pm to 8:40pm
+- **Topics:** 
+  - Program Architecture 
+  - .net core web API 
+  - Using Twilio for the texting service
+ 
+ ## October 23rd Meeting
+- **Time:** 8:00pm to 8:30pm
+- **Topics:** 
+  - Program Architecture 
+  - Acuity API
+ 
+ ## November 6th Meeting
+- **Time:** 8:00pm to 8:25pm
+- **Topics:** 
+  - Acuity API Data Processing 
+  - Twilio 
