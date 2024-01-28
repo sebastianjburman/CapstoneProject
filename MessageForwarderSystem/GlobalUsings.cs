85454517b55c6aab54fa558fbbcb51aeb7c9bdc0
Xiaokun Li <xli@bastiansolutions.com>
2024-01-27 20:56:16 -0500

    Set up versioning and cutomize api documentation #2

---
+++ MessageForwarderSystem/GlobalUsings.cs
@@ -0,0 +1,16 @@
+﻿global using MessageForwarderSystem.ApiVersionSupport;
+
+
+global using Microsoft.Extensions.Options;
+global using Microsoft.OpenApi.Any;
+global using Microsoft.OpenApi.Models;
+global using Swashbuckle.AspNetCore.Annotations;
+global using Swashbuckle.AspNetCore.SwaggerGen;
+global using System.Reflection;
+global using System.Text.Json;
+
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+global using MessageForwarderSystem.Swagger;
+global using MessageForwarderSystem.Swagger.Models;
--- MessageForwarderSystem/Program.cs
+++ MessageForwarderSystem/Program.cs
@@ -1,11 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    options.JsonSerializerOptions.WriteIndented = true;
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+}).ConfigureApiBehaviorOptions(options =>
+{
+    //suppress automatic model state binding errors
+    options.SuppressModelStateInvalidFilter = true;
+    options.SuppressMapClientErrors = false;
+    options.ClientErrorMapping[StatusCodes.Status404NotFound].Link =
+        "https://www.nklou.org/";
+    options.ClientErrorMapping[StatusCodes.Status404NotFound].Title =
+        "End point is invalid";
+});
+
+builder.Services.AddApiVersioning();
+builder.Services.AddAutoLotApiVersionConfiguration(new ApiVersion(1, 0));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckleteetsss
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddAndConfigureSwagger(
+    builder.Configuration,
+    Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"),
+    true);
+
+
 
 var app = builder.Build();
 
@@ -13,7 +42,18 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(
+        options =>
+        {
+            using var scope = app.Services.CreateScope();
+            var versionProvider = scope.ServiceProvider.GetRequiredService<IApiVersionDescriptionProvider>();
+            // build a swagger endpoint for each discovered API version
+            foreach (var description in versionProvider.ApiVersionDescriptions)
+            {
+                options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                    description.GroupName.ToUpperInvariant());
+            }
+        });
 }
 
 app.UseHttpsRedirection();
